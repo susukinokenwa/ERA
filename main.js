75f2431e2f2f37537b751c41997f6d9b237b5b8f
Brsrk <frostdaone@gmail.com>
2026-04-08 22:48:02 -0700

    3.3.4
    Changed Cursor Movement

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let mx = 0, my = 0, rx = 0, ry = 0;
 document.addEventListener('mousemove', e => {
   mx = e.clientX;
   my = e.clientY;
+  // remove cursor update from here entirely
 
   const cw = cursor.offsetWidth / 2;
   const ch = cursor.offsetHeight / 2;
@@ -13,8 +14,8 @@ document.addEventListener('mousemove', e => {
 });
 
 function animRing() {
-  rx += (mx - rx) * 0.12;
-  ry += (my - ry) * 0.12;
+  rx += (mx - rx) * 1;
+  ry += (my - ry) * 1;
 
   const rw = ring.offsetWidth / 2;
   const rh = ring.offsetHeight / 2;
